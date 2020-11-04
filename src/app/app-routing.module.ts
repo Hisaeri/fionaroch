13bdb38eca54e9658722a07017a30d64cd6525e5
Fiona Roch <roch.fiona@gmail.com>
2020-11-04 17:17:11 +0900

    load home as / routing + preload all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./modules/home/home.module')
+      .then((mod) => mod.HomeModule),
+  },
+  {
+    path: '**', redirectTo: '/',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
