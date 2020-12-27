4e56a24242e3f31efb941deee7a8546067f894ab
Fiona <roch.fiona@gmail.com>
2020-12-27 16:32:28 +0900

    clean up google analytics service

--- src/app/services/google-analytics.service.ts
+++ src/app/services/google-analytics.service.ts
@@ -18,10 +18,6 @@ export class GoogleAnalyticsService {
 
   constructor(private router: Router) { }
 
-  /** ********************************************************************************************
-  ******************************************************************************* PUBLIC METHODS
-  **********************************************************************************************/
-
   // Initialize analytics
   public init() {
     if (environment.production) {
@@ -30,34 +26,22 @@ export class GoogleAnalyticsService {
     }
   }
 
-  // Send events to all analytics services
-  public sendEvent(event: string, label: string = null,): void {
+  // Send events to analytics if on production mode
+  public sendEvent(event: string, label: string = null): void {
     const eventType: any = this.eventName[event];
-    if (environment.production) {
-      if (eventType) {
-        this.sendGoogleEvent(this.eventName[event].gCateg, this.eventName[event].gName, label);
-      } else {
-        console.error('ERROR: No event ', event);
-      }
+    if (environment.production && eventType) {
+        this.sendGoogleEvent(this.eventName[event].gCateg,
+          this.eventName[event].gName, label);
     }
   }
 
-  /** ********************************************************************************************
-  ******************************************************************************* PRIVATE METHODS
-  **********************************************************************************************/
-
   // Send event to google analytics
   private sendGoogleEvent(eventCategory: string, eventAction: string,
-    eventLabel: string = null, eventValue: number = null,
-    transaction_id: string = null): void {
+    eventLabel: string = null): void {
     const gData: any = {
       'event_category': eventCategory,
-      'event_label': eventLabel,
-      'value': eventValue,
+      'event_label': eventLabel
     };
-    if (transaction_id) {
-      gData.transaction_id = transaction_id;
-    }
     gtag('event', eventAction, gData);
   }
 
@@ -76,20 +60,21 @@ export class GoogleAnalyticsService {
   private initGoogleAnalytics(): void {
     try {
       // Add google tag manager script to page
-      const script1 = document.createElement('script');
-      script1.async = true;;
-      script1.src = 'https://www.googletagmanager.com/gtag/js?id=' + environment.googleAnalyticsId;
-      document.head.appendChild(script1);
+      const gScript = document.createElement('script');
+      gScript.async = true;
+      gScript.src = 'https://www.googletagmanager.com/gtag/js?id='
+      + environment.googleAnalyticsId;
+      document.head.appendChild(gScript);
 
       // Init google script
-      const script2 = document.createElement('script');
-      script2.innerHTML = `
+      const gInitScript = document.createElement('script');
+      gInitScript.innerHTML = `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', '` + environment.googleAnalyticsId + `', {'send_page_view': true});
-      `;
-      document.head.appendChild(script2);
+        gtag('config', '` + environment.googleAnalyticsId +
+        `', {'send_page_view': true});`;
+      document.head.appendChild(gInitScript);
     } catch (ex) {
       console.error('Error : Couldn\'t initialize google analytics');
       console.error(ex);
