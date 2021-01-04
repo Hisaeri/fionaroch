9e48bb7ed56c1932ed8d5520e482ee57bf0729b8
Fiona <roch.fiona@gmail.com>
2021-01-04 17:22:08 +0900

    add skills class & ionic to skills component

--- src/app/modules/skills/skills.component.ts
+++ src/app/modules/skills/skills.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+class Skill {
+  title: string;
+  path: string;
+
+  constructor(title: string, path: string) {
+    this.title = title;
+    this.path = path;
+  }
+};
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
@@ -8,8 +18,22 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SkillsComponent implements OnInit {
-  public frontSkills: Array<string> = ['angular', 'tscript', 'vue', 'sass', 'bootstrap'];
-  public backSkills: Array<string> = ['nodejs', 'pgsql', 'aws', 'heroku', 'django'];
+  public frontSkills: Array<Skill> = [
+    new Skill('Angular', '/assets/img/logos/angular.png'),
+    new Skill('VueJS', '/assets/img/logos/vue.png'),
+    new Skill('Ionic', '/assets/img/logos/ionic.png'),
+    new Skill('TypeScript', '/assets/img/logos/tscript.png'),
+    new Skill('Sass', '/assets/img/logos/sass.png'),
+    new Skill('Bootstrap', '/assets/img/logos/bootstrap.png'),
+  ];
+  public backSkills: Array<Skill> = [
+    new Skill('NodeJS', '/assets/img/logos/nodejs.png'),
+    new Skill('Postgresql', '/assets/img/logos/pgsql.png'),
+    new Skill('Amazon Web Services', '/assets/img/logos/aws.png'),
+    new Skill('Heroku', '/assets/img/logos/heroku.png'),
+    new Skill('Django', '/assets/img/logos/django.png'),
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
