5ee7de2614af57e9e60fc0eb459b708abe3fe679
Fiona <roch.fiona@gmail.com>
2021-01-14 16:56:38 +0900

    contact is now a comp

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ContactRoutingModule } from './contact-routing.module';
+// import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [ContactComponent],
   imports: [
     CommonModule,
-    ContactRoutingModule
-  ]
+    MatCardModule
+    // ContactRoutingModule
+  ],
+  exports: [ContactComponent]
 })
 export class ContactModule { }
