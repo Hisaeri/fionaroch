c4e10550515b41fd81d4c80c34c7a5522d2ad0c8
Fiona <roch.fiona@gmail.com>
2021-01-14 16:57:05 +0900

    add contact comp on home page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home.component';
 import { MatButtonModule } from '@angular/material/button'
 import { SkillsModule } from '../skills/skills.module';
 import { HeroModule } from '../hero/hero.module';
+import { ContactModule } from '../contact/contact.module';
 
 @NgModule({
   declarations: [HomeComponent],
@@ -15,7 +16,8 @@ import { HeroModule } from '../hero/hero.module';
     HomeRoutingModule,
     MatButtonModule,
     SkillsModule,
-    HeroModule
+    HeroModule,
+    ContactModule
   ]
 })
 export class HomeModule { }
