1b029f262c6aa2e93dcd1b90d5130dfa3da9332b
Fiona <roch.fiona@gmail.com>
2021-01-14 16:57:17 +0900

    remove about project contact routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'about', loadChildren: () => import('./modules/about/about.module')
-      .then((mod) => mod.AboutModule),
-  },
-  {
-    path: 'contact', loadChildren: () => import('./modules/contact/contact.module')
-      .then((mod) => mod.ContactModule),
-  },
-  {
-    path: 'projects', loadChildren: () => import('./modules/projects/projects.module')
-      .then((mod) => mod.ProjectsModule),
-  },
+  // {
+  //   path: 'about', loadChildren: () => import('./modules/about/about.module')
+  //     .then((mod) => mod.AboutModule),
+  // },
+  // {
+  //   path: 'contact', loadChildren: () => import('./modules/contact/contact.module')
+  //     .then((mod) => mod.ContactModule),
+  // },
+  // {
+  //   path: 'projects', loadChildren: () => import('./modules/projects/projects.module')
+  //     .then((mod) => mod.ProjectsModule),
+  // },
   {
     path: '', loadChildren: () => import('./modules/home/home.module')
       .then((mod) => mod.HomeModule),
