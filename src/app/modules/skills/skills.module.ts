a92e85ffd78ec819dec74df5a6b483310a165d24
Fiona <roch.fiona@gmail.com>
2021-01-16 17:22:20 +0900

    update skills design

--- src/app/modules/skills/skills.module.ts
+++ src/app/modules/skills/skills.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkillsComponent } from './skills.component';
 import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [SkillsComponent],
   imports: [
     CommonModule,
-    MatCardModule,
-    MatDividerModule
+    MatCardModule
   ],
   exports: [SkillsComponent]
 })
